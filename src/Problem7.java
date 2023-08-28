679594c16f0af41e12061c3495ca3dd60099fcd8
plukka <plukka@expediagroup.com>
2023-08-28 10:46:27 -0700

    problem 6, 7

--- src/Problem7.java
+++ src/Problem7.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem7 {
     // What is Nth Prime Number
     // 2,3,5,7,11,13-> 6th Prime number is 13.
@@ -5,8 +8,11 @@ public class Problem7 {
     public static void main(String[] args) {
         System.out.println(nthPrimeNumber(10001));
         System.out.println(nthPrimeNumber(6));
+        System.out.println(nthPrimer(10001));
+        System.out.println(nthPrimer(6));
     }
 
+    // Optimized way of implementation
     static long nthPrimeNumber(int n) {
         long startTime = System.currentTimeMillis();
         if (n == 1)
@@ -14,11 +20,38 @@ public class Problem7 {
         long primeNumber = 1, count = 1;
         while (count != n) {
             primeNumber += 2; // even numbers are not prime.
-            if (Utility.isPrimeNumber(primeNumber))
+            // if (Utility.isPrimeNumber(primeNumber)) -> takes lot of time.
+            if (Utility.isPrime(primeNumber) == 1)
                 count++;
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("Time taken is " +  (endTime - startTime));
+        System.out.println("Time taken is " + (endTime - startTime));
         return primeNumber;
+    }
+
+    // Taking more time with array approach.
+    static long nthPrimer(int n) {
+        long startTime = System.currentTimeMillis();
+        int MAX_SIZE = 1000005;
+        boolean[] isPrimes = new boolean[MAX_SIZE];
+        for (int i = 0; i < MAX_SIZE; i++) {
+            isPrimes[i] = true;
+        }
+        for (int p = 2; p * p < MAX_SIZE; p++) {
+            // if isPrimes[p] is not changed then it is prime
+            if (isPrimes[p] == true) {
+                // Update all multiples of p greater than or equal to the square of it
+                // numbers which are multiple of p and are less than p^2 are already been marked.
+                for (int i = p * p; i < MAX_SIZE; i += p)
+                    isPrimes[i] = false;
+            }
+        }
+        List<Integer> primes = new ArrayList<Integer>();
+        for (int p = 2; p < MAX_SIZE; p++)
+            if (isPrimes[p] == true)
+                primes.add(p);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken is " + (endTime - startTime));
+        return primes.get(n-1);
     }
 }
--- src/Utility.java
+++ src/Utility.java
@@ -8,4 +8,21 @@ public class Utility {
 
         return true;
     }
+
+    public static int isPrime(long n) {
+        // corner case
+        if (n <= 1) return 0;
+        if (n == 2 || n == 3) return 1;
+
+        // below 5 there is only two prime numbers 2 and 3
+        if (n % 2 == 0 || n % 3 == 0)
+            return 0;
+
+        // concept of prime number can be represented in form of (6*k + 1) or(6*k - 1)
+        for (int i = 5; i * i <= n; i = i + 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return 0;
+        }
+        return 1;
+    }
 }
